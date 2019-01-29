097cad1f8e078abeff7db84c3a85b1b3ec4d20eb
Eva Maria Perez Fernandez <evamp@beta19.aulas.gsyc.urjc.es>
2019-01-29 12:40:38 +0100

    Ultima vers

--- Practica-1/web1/server.js
+++ Practica-1/web1/server.js
@@ -4,6 +4,6 @@ console.log("Arrancando servidor...")
 
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
+  res.end('Hello Eva!');
   console.log("Peticion atendida")
 }).listen(8080);
