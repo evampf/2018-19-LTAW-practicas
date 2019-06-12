7af0c4554dbf5bab90c8a73ffa67b1770c8d3efb
Eva Maria Perez Fernandez <evam@MacBook-Pro-de-EvaM.local>
2019-06-12 19:13:02 +0200

    "modificación index.html"

--- Practica-1/mitienda.js
+++ Practica-1/mitienda.js
@@ -1,63 +1,43 @@
+// SERVER.js
+// This file opens the request file and returns the content to the client.
+// If anything goes wrong, throw a 404 error.
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
 
 console.log("Arrancando servidor...")
 
-//-- Configurar y lanzar el servidor. Por cada peticion recibida
-//-- se imprime un mensaje en la consola
-http.createServer((req, res) => {
-  console.log("---> Peticion recibida")
-  console.log("Recurso solicitado (URL): " + req.url)
+//req, res: PETICIÓN, RESPUESTA:
+http.createServer(function (req, res) {
+  console.log("--> Petición RECIBIDA!");
+
   var q = url.parse(req.url, true);
-  console.log("URL parseada: ")
-  console.log("Host: " + q.host)
-  console.log("pathname:" + q.pathname)
+  var filename = "." + q.pathname; //filename: ./xxx.// XXX:
 
-  //-- Obtener el fichero. Si es "/" se toma index.html
-  //-- Poner el "." delante para que sean un fichero del directorio actual
-
-  var filename = ""
-
-  if (q.pathname == "/")
-    filename += "/index.html"
-  else {
-    filename = q.pathname
+  if (q.pathname == "/"){
+    filename += "/index.html";
   }
 
-  //-- Obtener el tipo de fichero segun la extension
-  tipo = filename.split(".")[1]
+  fs.readFile(filename, function(err, data){
+    console.log("Recurso solicitado(URL): " + filename);
+    console.log();
 
-  //-- Obtener el nombre del fichero a partir del recurso solicitado
-  //-- Se añade un . delante
-  filename = "." + filename
-
-  console.log("Filename: " + filename)
-  console.log("Tipo: " + tipo)
-
-  fs.readFile(filename, function(err, data) {
-    if (err) {
+    if (err){
       res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
+      res.end(" 404   NOT FOUND");
+    } else {
+      // Tipo MIME por defecto:
+      //var mime = "text/html";
+
+      // Mensaje de respuesta:
+      //res.writeHead(200, {'Content-Type': 'mime'});
+      res.write(data);
+      res.end();
     }
-
-    //-- Tipo mime por defecto: html
-    var mime = "text/html"
-
-    //-- Es una imagen
-    if (['png', 'jpg'].includes(tipo)) {
-      console.log("IMAGEN!!!!!")
-      mime = "image/" + tipo
-    }
-
-    //-- Es un css
-    if (tipo == "css")
-      mime = "text/css"
-
-    //-- Generar el mensaje de respuesta
-    res.writeHead(200, {'Content-Type': mime});
-    res.write(data);
-    res.end();
   });
-
 }).listen(8080);
+
+// NAVEGADOR LOCAL: http://localhost:8080/index.html
+
+// Imprimir en la misma URL:
+//res.end(req.url);
