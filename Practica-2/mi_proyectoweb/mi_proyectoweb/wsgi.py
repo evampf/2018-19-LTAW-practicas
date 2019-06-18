e944c332dfd3b172205203c2bfbe08ac1c752dd2
Eva Maria Perez Fernandez <evamp@alpha22.aulas.gsyc.urjc.es>
2019-06-18 11:56:52 +0200

    "No reconoce css"

--- Practica-2/mi_proyectoweb/mi_proyectoweb/wsgi.py
+++ Practica-2/mi_proyectoweb/mi_proyectoweb/wsgi.py
@@ -1,8 +1,10 @@
 """
 WSGI config for mi_proyectoweb project.
+
 It exposes the WSGI callable as a module-level variable named ``application``.
+
 For more information on this file, see
-https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
+https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
